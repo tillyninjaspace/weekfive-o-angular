5209f054cc145776445a5ee25f87318542f264ca
tillyninjaspace <squealingpet@gmail.com>
2021-05-13 09:43:01 -0700

    splice(0,0, post)

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import {HttpClient} from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { PromiseType } from 'protractor/built/plugins';
 
 @Component({
   selector: 'posts',
@@ -8,14 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent {
   posts: any = [];
- 
-  constructor(http: HttpClient) { 
+  private url = 'http://jsonplaceholder.typicode.com/posts'
+
+  constructor(private http: HttpClient) { 
    
-    http.get('http://jsonplaceholder.typicode.com/posts')
+    http.get(this.url)
     .subscribe(response => {
       console.log(response);
       this.posts = response;
     });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post: any = {title: input.value}
+    input.value = '';
+
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        this.posts.splice(0,0, post)
+        console.log(response)
+      })
   }
 
   ngOnInit(): void {
